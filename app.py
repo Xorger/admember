63558d38abd5717ee7a921ca50cea6ec4c0d2fc8
Elijah Yajaman <112749251+Xorger@users.noreply.github.com>
2024-10-08 19:33:19 +1000

    got sql setup

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
+import os
 from flask import Flask, render_template
+import sqlite3
 
-app = Flask(__name__)
+database_filename = "database.db"
+
+sql_statements = [
+    """CREATE TABLE IF NOT EXISTS users (
+            user_id integer AUTOINCREMENT PRIMARY KEY,
+            username text NOT NULL
+            name text NOT NULL
+            password_hash text NOT NULL
+            status integer NOT NULL DEFAULT FALSE
+            admin integer NOT NULL DEFAULT FALSE
+            );"""
+]
+
+with sqlite3.connect(database_filename) as db:
+    cursor = db.cursor
+    for statement in sql_statements:
+        cursor.execute(statement)
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
