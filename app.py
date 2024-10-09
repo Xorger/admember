098f26490c2a9ef94a6e499ac0d90dfe341d782f
Elijah Yajaman <112749251+Xorger@users.noreply.github.com>
2024-10-09 19:41:04 +1000

    got index page basic structure

--- app.py
+++ app.py
@@ -2,24 +2,30 @@ import os
 from flask import Flask, render_template
 import sqlite3
 
+app = Flask(__name__)
+
 database_filename = "database.db"
 
 sql_statements = [
     """CREATE TABLE IF NOT EXISTS users (
-            user_id integer AUTOINCREMENT PRIMARY KEY,
-            username text NOT NULL
-            name text NOT NULL
-            password_hash text NOT NULL
-            status integer NOT NULL DEFAULT FALSE
-            admin integer NOT NULL DEFAULT FALSE
-            );"""
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    name TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    status BOOLEAN NOT NULL DEFAULT FALSE,
+    admin BOOLEAN NOT NULL DEFAULT FALSE
+    );"""
 ]
 
 with sqlite3.connect(database_filename) as db:
-    cursor = db.cursor
+    cursor = db.cursor()
     for statement in sql_statements:
         cursor.execute(statement)
+    db.commit()
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    with sqlite3.connect(database_filename) as db:
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM users")
+    return render_template("index.html", rows=cursor.fetchall())
